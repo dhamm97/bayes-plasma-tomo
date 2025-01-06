e8a8a8a2a7dc0b90d49323ceeb18dba315650d0d
dhamm97 <daniele.hamm@epfl.ch>
2025-01-06 13:00:31 +0100

    repo refactoring part 1

--- src/results/dataset_generation/ula_study.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 0 9);
-do
-    python ula_iteration_number_tuning.py $((i * 10 + 900)) $(((i+1) * 10 + 900)) &
-done
--- src/results/dataset_generation/uq_study.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 0 9);
-do
-    python uq_study.py $((i * 90)) $(((i+1) * 90))  &
-done
