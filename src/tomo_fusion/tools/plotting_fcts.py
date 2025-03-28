c825a170b06201a2b42a20f79caec3df42be7f5b
dhamm97 <daniele.hamm@epfl.ch>
2025-03-28 12:54:44 +0100

    updating results generating notebooks

--- src/tomo_fusion/tools/plotting_fcts.py
+++ src/tomo_fusion/tools/plotting_fcts.py
@@ -124,11 +124,13 @@ def plot_profile(image,
                  tcv_plot_clip=False,
                  contour_image=None, levels=15,
                  ax=None, colorbar=False,
-                 interpolation=None, vmin=None, vmax=None, cmap="viridis", contour_color="w", aspect=None):
+                 interpolation=None, vmin=None, vmax=None, cmap="viridis", contour_color="w", aspect=None,
+                 peak_stats=None,
+                 pad_cbar=0, cbar_tick_params=None):
     if tcv_plot_clip:
         # define TCV patch for plotting
         tcv_shape_coords = np.load(dirname + "/../forward_model/tcv_shape_coords.npy")
-        Lr, Lz = 0.511, 1.5
+        Lr, Lz = 0.511 if peak_stats is None else 0.5, 1.5
         h = Lz / image.shape[0]
         zs = np.linspace(0, Lz, round(Lz / h), endpoint=False) + 0.5 * h
         rs = np.linspace(0, Lr, round(Lr / h), endpoint=False) + 0.5 * h
@@ -136,6 +138,24 @@ def plot_profile(image,
         tcv_shape_coords[:, 1] = tcv_shape_coords[:, 1] * zs.size - 0.5 * h
         path = Path(tcv_shape_coords.tolist())
         patch = PathPatch(path, facecolor='none')
+
+    handle = plt if ax is None else ax
+    if peak_stats is not None:
+        handle.plot(peak_stats["true_loc"][1], peak_stats["true_loc"][0], 'r.', markersize=2*peak_stats["markersize"])
+        handle.plot(peak_stats["mean"][1], peak_stats["mean"][0], 'k.', markersize=peak_stats["markersize"])
+        lower_bound_hor, upper_bound_hor = (
+        peak_stats["mean"][1] - peak_stats["nb_stds"] * peak_stats["std"][1],
+        peak_stats["mean"][1] + peak_stats["nb_stds"] * peak_stats["std"][1])
+        lower_bound_vert, upper_bound_vert = (
+        peak_stats["mean"][0] - peak_stats["nb_stds"] * peak_stats["std"][0],
+        peak_stats["mean"][0] + peak_stats["nb_stds"] * peak_stats["std"][0])
+        print(peak_stats["mean"])
+        print(lower_bound_hor,upper_bound_hor,lower_bound_vert,upper_bound_vert  )
+        handle.plot(np.array([lower_bound_hor, lower_bound_hor]), np.array([lower_bound_vert, upper_bound_vert]), 'k', linewidth=peak_stats["linewidth"])#, dashes=(1.5, 1.5))
+        handle.plot(np.array([upper_bound_hor, upper_bound_hor]), np.array([lower_bound_vert, upper_bound_vert]), 'k', linewidth=peak_stats["linewidth"])#, dashes=(1.5, 1.5))
+        handle.plot(np.array([lower_bound_hor, upper_bound_hor]), np.array([lower_bound_vert, lower_bound_vert]), 'k', linewidth=peak_stats["linewidth"])#, dashes=(1.5, 1.5))
+        handle.plot(np.array([lower_bound_hor, upper_bound_hor]), np.array([upper_bound_vert, upper_bound_vert]), 'k', linewidth=peak_stats["linewidth"])#, dashes=(1.5, 1.5))
+
 
     # plot clipping to tcv shape
     if ax is None:
@@ -146,16 +166,16 @@ def plot_profile(image,
             lcms_level = np.where(c.levels == 0)[0][0]
             c.collections[lcms_level].set_linewidth(0.75)
         p = plt.imshow(image, interpolation=interpolation, vmin=vmin, vmax=vmax, cmap=cmap, aspect=aspect)
-        if tcv_plot_clip:
-            plt.gca().add_patch(patch)
-            p.set_clip_path(patch)
-            if contour_image is not None:
-                c.set_clip_path(patch)
-        else:
-            plt.imshow(image, interpolation=interpolation, vmin=vmin, vmax=vmax, aspect=aspect)
-        plt.axis('off')
-        if colorbar:
-            plt.colorbar()
+        # if tcv_plot_clip:
+        #     plt.gca().add_patch(patch)
+        #     p.set_clip_path(patch)
+        #     if contour_image is not None:
+        #         c.set_clip_path(patch)
+        #else:
+        #    plt.imshow(image, interpolation=interpolation, vmin=vmin, vmax=vmax, aspect=aspect)
+        # plt.axis('off')
+        # if colorbar:
+        #     plt.colorbar()
     else:
         # plot on given figure axis
         if contour_image is not None:
@@ -164,16 +184,36 @@ def plot_profile(image,
             lcms_level = np.where(c.levels == 0)[0][0]
             c.collections[lcms_level].set_linewidth(0.75)
         p = ax.imshow(image, interpolation=interpolation, vmin=vmin, vmax=vmax, cmap=cmap, aspect=aspect)
-        if tcv_plot_clip:
-            ax.add_patch(patch)
-            p.set_clip_path(patch)
-            if contour_image is not None:
-                c.set_clip_path(patch)
+        # if tcv_plot_clip:
+        #     ax.add_patch(patch)
+        #     p.set_clip_path(patch)
+        #     if contour_image is not None:
+        #         c.set_clip_path(patch)
+        #else:
+        #    p = ax.imshow(image, interpolation=interpolation, aspect=aspect, vmin=vmin, vmax=vmax)
+        # ax.axis('off')
+        # if colorbar:
+        #     plt.colorbar(p, ax=ax)
+    #plt.autoscale(False)
+
+    if tcv_plot_clip:
+        hax = plt.gca() if ax is None else ax
+        hax.add_patch(patch)
+        p.set_clip_path(patch)
+        if contour_image is not None:
+           c.set_clip_path(patch)
+
+    handle.axis('off')
+    if colorbar:
+        if ax is None:
+            cbar = plt.colorbar()
         else:
-            p = ax.imshow(image, interpolation=interpolation, aspect="auto", vmin=vmin, vmax=vmax)
-        ax.axis('off')
-        if colorbar:
-            plt.colorbar(p, ax=ax)
+            cbar = plt.colorbar(p, ax=ax, pad=pad_cbar)
+        if cbar_tick_params is not None:
+            cbar.ax.tick_params(labelsize=cbar_tick_params["labelsize"])
+            cbar.ax.set_yticks(cbar_tick_params["yticks"])
+            cbar.ax.set_yticklabels(cbar_tick_params["yticklabels"])
+
     return
 
 
@@ -329,7 +369,42 @@ def plot_uq_data(uq_data, ground_truth, psi, levels=12,
     plt.show()
 
 
-
+def plot_ptheta_LoS_tcv(params, markersize=5):
+    """
+    This function plots, for a given (p, theta) configuration, the LoS configuration
+    taking into account the TCV geometry. The shaded areas correspond to lines that fall
+    outside the vessel.
+    """
+    tcv_mask_finesse = round(300)
+    pmin = np.min(params[:, 0])
+    prange = np.max(params[:, 0]) - np.min(params[:, 0])
+    # pmax corresponding to tcv geometry
+    tcv_pmax = 0.5646 + 0.25
+    PT_intersecting_tcv_mask = np.load(dirname+'/../forward_model/tcv_mask_sinogram.npy')
+    plt.figure(figsize=(6,3))
+    plt.rcParams.update({
+        "text.usetex": True,
+        "font.family": "Helvetica"
+    })
+    # plot tcv mask
+    plt.imshow(PT_intersecting_tcv_mask, origin="upper", aspect="auto", cmap="gray", alpha=0.5)
+    # plot considered LoS configuration
+    scaling_factor_p = prange / (2*tcv_pmax)  # 2*pmax/(...)
+    scaling_factor_theta = np.max(params[:, 1]) / np.pi
+    ps = (params[:, 0]+np.abs(np.min(params[:, 0])))/np.max(params[:, 0]+np.abs(np.min(params[:, 0])))*(tcv_mask_finesse-1)*scaling_factor_p
+    # shift ps
+    #ps += eps / (2*pmax + 2*eps) * (tcv_mask_finesse-1)
+    ps += (tcv_pmax + pmin) / (2 * tcv_pmax) * (tcv_mask_finesse-1)
+    thetas = params[:, 1]/np.max(params[:, 1])*(tcv_mask_finesse-1)*scaling_factor_theta
+    for i in range(params.shape[0]):
+        #a=0
+        plt.plot(thetas[i], ps[i], "r", marker=".", markersize=markersize)
+    plt.xticks([0, tcv_mask_finesse/2, tcv_mask_finesse-1], [r'$0$', r'$\pi/2$', r'$\pi$'], fontsize=20)
+    plt.yticks([0, tcv_mask_finesse/2, tcv_mask_finesse-1], [r'$-p_{max}$', r'$0$', r'$p_{max}$'], fontsize=20)
+    #plt.yticks([])
+    plt.xlabel(r"$\theta$", fontsize=25)
+    plt.ylabel(r"$p$", rotation=0, fontsize=25, labelpad=10)
+    plt.title("Subsampled sinogram", fontsize=25, color="k")
 
 
 
