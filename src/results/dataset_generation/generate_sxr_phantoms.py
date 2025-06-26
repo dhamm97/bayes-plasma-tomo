31136d5abe026a044a06091338f97d78cd44d6e0
dhamm97 <daniele.hamm@epfl.ch>
2025-06-26 14:07:30 +0200

    clean up phantom gen script

--- src/results/dataset_generation/generate_sxr_phantoms.py
+++ src/results/dataset_generation/generate_sxr_phantoms.py
@@ -42,8 +42,8 @@ def generate_sxr_samples(psi_fct, dim_shape=(1, 120, 40), sampling=0.0125,
     # trimming limits
     upper_t = 37
     lower_t = 27
-    hfs_t = 5  # 4
-    lfs_t = 3  # 2
+    hfs_t = 5
+    lfs_t = 3
     trimming_values = np.zeros((nsamples, 4))
     source_loc = np.zeros((nsamples, 2), dtype=int)
     psis_at_source = np.zeros((nsamples))
@@ -90,46 +90,19 @@ def generate_sxr_samples(psi_fct, dim_shape=(1, 120, 40), sampling=0.0125,
         psi_at_source = psi[mask_locs[0][sources_idx[0]], mask_locs[1][sources_idx[0]]]
         psis_at_source[i] = psi_at_source
 
-        source_loc_bins = np.linspace(psi_lowerbound_source, psi_upperbound_source, 6, endpoint=True)
-        # alpha_bounds = np.array([[np.log(1e-2), np.log(5e-1)],
-        #                          [np.log(5e-3), np.log(5e-2)],
-        #                          [np.log(5e-3), np.log(5e-2)],  # 1e-2
-        #                          [np.log(1e-3), np.log(5e-2)],  # 5e-3
-        #                          [np.log(5e-4), np.log(5e-2)]])  # 5e-3
-        # diffusive_steps_bounds = steps_nb_factor * np.array([[int(5e3), int(1e4)],
-        #                                    [int(1e3), int(1e4)],
-        #                                    [int(5e2), int(5e3)],
-        #                                    [int(5e2), int(5e3)],
-        #                                    [int(5e2), int(5e3)]])
-        # for bin in range(source_loc_bins.size - 1):
-        #     if psi_at_source >= source_loc_bins[bin] and psi_at_source < source_loc_bins[bin + 1]:
-        #         alpha_random_values[i] = np.exp(np.random.uniform(alpha_bounds[bin, 0],
-        #                                                           alpha_bounds[bin, 1]))
-        #         if alpha_random_values[i] < 5e-3:
-        #             lower_bound_diffusive_steps = steps_nb_factor * int(3e3)
-        #         else:
-        #             lower_bound_diffusive_steps = diffusive_steps_bounds[bin, 0]
-        #         diffusive_steps[i] = np.random.randint(lower_bound_diffusive_steps,
-        #                                                diffusive_steps_bounds[bin, 1])
         # New definition of bounds
         alpha_random_values[i] = np.exp(np.random.uniform(alpha_bounds[0], alpha_bounds[1]))
         diffusive_steps[i] = np.random.randint(diffusive_steps_bounds[0], diffusive_steps_bounds[1])
 
-                # if we need to clip outside core, define core mask_core
+        # if we need to clip outside core, define core mask_core
         if clipping_outside_core:
             mask_core = define_core_mask(psi, dim_shape[1:],
                                              trim_values_x=[trimming_values[i, 0], trimming_values[i, 1]])
             mask_core = np.expand_dims(mask_core, 0)
-            # plt.imshow(mask_core.squeeze())
-            # plt.colorbar()
-            # plt.contour(psi, origin="lower", levels=15, antialiased=True, colors="r", linewidths=0.2)
-            # plt.show()
 
         if gpu:
             psi = cp.array(psi)
 
-        #breakpoint()
-        #st = time.time()
         if reg_fct_type == "coherence_enhancing":
             reg_fct = AnisCoherenceEnhancingDiffusionOp(dim_shape=dim_shape, alpha=alpha_random_values[i], m=1,
                                                         sigma_gd_st=sigma_gd_st_factor * sampling, smooth_sigma_st=smooth_sigma_st_factor * sampling,
@@ -144,20 +117,11 @@ def generate_sxr_samples(psi_fct, dim_shape=(1, 120, 40), sampling=0.0125,
                                            matrix_based_impl=True,
                                            gpu=gpu, dtype=psi_fct.dtype)
 
-        #print(reg_fct._grad_matrix_based.mat.dtype, type(reg_fct._grad_matrix_based.mat))
-        #reg_fct._grad_matrix_based.mat = reg_fct._grad_matrix_based.mat.astype(Bfield.dtype)
-        #reg_fct._grad_matrix_based.mat = sp.csr_matrix(reg_fct._grad_matrix_based.mat).
-
-        #print("instantiation-time", time.time()-st)
-        #st=time.time()
-
         # print information
         print("Phantom {}: ".format(i))
         print(psi_at_source, alpha_random_values[i], nsources, diffusive_steps[i])
         print("\n")
 
-        # generate sample, adapting number of diffusive steps for "extreme" initialization locations
-        #x0 = np.expand_dims(x0, 0)
         x0 = x0.flatten()
         if clipping_outside_core:
             mask_core = mask_core.flatten()
@@ -165,27 +129,17 @@ def generate_sxr_samples(psi_fct, dim_shape=(1, 120, 40), sampling=0.0125,
             x0 = cp.array(x0)
             if clipping_outside_core:
                 mask_core = cp.array(mask_core)
-        #print("loading-time", time.time()-st)
         for s in range(diffusive_steps[i]):
             # 2/L with L=8 seems enough, L=8 good estimate of diff_lipschitz constant of reg_fct in principle..
-            #x0 -= 0.95 * (0.25 * (sampling ** 2)) * reg_fct.grad(x0)
             x0 -= 0.95 * (0.25 * (sampling ** 2)) * reg_fct._grad_matrix_based.mat.dot(x0)
-            #x0 -= 0.95 * (0.25 * (sampling ** 2)) * reg_fct._grad_matrix_based.mat@x0
             if clip_each_iter:
                 x0[x0 < 0] = 0
             if clipping_outside_core:
                 x0 *= mask_core
-            # if s % 1000 == 0:
-            #     plt.imshow(x0.squeeze())
-            #     plt.colorbar()
-            #     plt.contour(psi, origin="lower", levels=15, antialiased=True, colors="r", linewidths=0.2)
-            #     plt.title("it {}".format(s))
-            #     plt.show()
         x0[x0 < 0] = 0
         if clipping_outside_core:
             x0 *= mask_core
 
-        #print("loop-time", time.time() - st)
         # normalize sample to 1
         x0 /= x0.max()
         # append sample and magnetic equilibrium
@@ -199,9 +153,8 @@ def generate_sxr_samples(psi_fct, dim_shape=(1, 120, 40), sampling=0.0125,
                                                      mask_core.reshape(dim_shape[1], dim_shape[2]), peak_values_gaussian_background[i])
         sxr_samples_with_background.append(x0_plus_background)
         psis.append(psi)
-        #print("loop-get-time", time.time()-st)
 
-        # # save information
+        # save information
         if save:
             import os
             save_dir = save_dir
@@ -216,8 +169,6 @@ def generate_sxr_samples(psi_fct, dim_shape=(1, 120, 40), sampling=0.0125,
             np.save(save_dir + '/trimming_values.npy', trimming_values)
             np.save(save_dir + '/source_loc.npy', source_loc)
             np.save(save_dir + '/peak_values_gaussian_background.npy', peak_values_gaussian_background)
-        #print("loop-get-save-time", time.time()-st)
-        #breakpoint()
     return sxr_samples, psis
 
 
@@ -237,49 +188,10 @@ if __name__ == '__main__':
     min_peak_value = 0.2
     peak_vals = min_peak_value + (1-min_peak_value) * np.random.rand(nsamples)
 
-    # _, _ = generate_sxr_samples(psi_eq, dim_shape=(1, 240, 80),
-    #                                                      reg_fct_type="coherence_enhancing",
-    #                                                      sigma_gd_st_factor=0, smooth_sigma_st_factor=2,
-    #                                                      sampling=0.00625, steps_nb_factor=2,
-    #                                                 nsamples=nsamples, seed=0, save=True, save_dir="sxr_samples_fine_discretization_new",
-    #                                                 clip_each_iter=True, clipping_outside_core=True,
-    #                                                 diff_method_struct_tens="fd", gpu=args.gpu,
-    #                                                 peak_values_gaussian_background=peak_vals)
-    #
-    # _, _ = generate_sxr_samples(psi_eq, dim_shape=(1, 120, 40),
-    #                                                      reg_fct_type="coherence_enhancing",
-    #                                                      sigma_gd_st_factor=0, smooth_sigma_st_factor=1,
-    #                                                      sampling=0.0125,
-    #                                                 nsamples=nsamples, seed=0, save=True, save_dir="sxr_samples_new",
-    #                                                 clip_each_iter=True, clipping_outside_core=True,
-    #                                                 diff_method_struct_tens="fd", gpu=args.gpu,
-    #                                                 peak_values_gaussian_background=peak_vals)
-
     _, _ = generate_sxr_samples(psi_eq, dim_shape=(1, 240, 80),
                                                          reg_fct_type="anisotropic",
                                                          sampling=0.00625, steps_nb_factor=4,
-                                                    nsamples=nsamples, seed=0, save=True, save_dir="sxr_samples_fine_anisotropic_new_bounds",
+                                                    nsamples=nsamples, seed=0, save=True, save_dir="sxr_samples",
                                                     clip_each_iter=True, clipping_outside_core=True,
                                                     diff_method_struct_tens="fd", gpu=args.gpu,
                                                     peak_values_gaussian_background=peak_vals)
-
-    _, _ = generate_sxr_samples(psi_eq, dim_shape=(1, 120, 40),
-                                                         reg_fct_type="anisotropic",
-                                                         sampling=0.0125,
-                                                    nsamples=nsamples, seed=0, save=True, save_dir="sxr_samples_anisotropic_new_bounds",
-                                                    clip_each_iter=True, clipping_outside_core=True,
-                                                    diff_method_struct_tens="fd", gpu=args.gpu,
-                                                    peak_values_gaussian_background=peak_vals)
-
-
-
-
-# fig,ax=plt.subplots(4,5, figsize=(6,16))
-# for i in range(4):
-#     for j in range(5):
-#         im=ax[i,j].imshow(samples_solps[i*5+j])
-#         ax[i, j].contour(psis_solps[i*5+j], origin="lower", levels=15, antialiased=True, colors="r", linewidths=0.2)
-#         #plt.colorbar(im, ax=ax[i,j])
-#         ax[i,j].set_title("{}".format(i*5+j))
-#         ax[i,j].axis('off')
-# plt.show()
